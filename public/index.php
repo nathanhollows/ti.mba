104b90c098a6837a62d285e8e1f700382e056d79
wisend <nathanhollows@gmail.com>
2015-10-13 20:06:15 +1300

    Added base files

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\DI\FactoryDefault;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter; 
