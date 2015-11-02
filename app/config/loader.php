5b7b8d43826ee388e79794a8efb232e774cfaeff
wisend <me@nathanhollows.com>
2015-11-03 07:39:42 +1300

    Config
    Load new directories, classes

--- app/config/loader.php
+++ app/config/loader.php
@@ -5,6 +5,27 @@ $loader = new \Phalcon\Loader();
 /**
  * We're a registering a set of directories taken from the configuration file
  */
-$loader->registerNamespaces(array(
-	'App\Controllers' => __DIR__ . '/../controllers/'
-))->register();
+$loader->registerNamespaces(
+	array(
+		'App\Controllers' => __DIR__ . '/../controllers/'
+	)
+)->register();
+
+// Register some classes
+$loader->registerClasses(
+    array(
+        'Elements'		=> __DIR__ . '/../library/Elements.php'
+    )
+)->register();
+
+$loader->registerDirs(
+	array(
+		__DIR__ . $config->application->controllersDir,
+		__DIR__ . $config->application->pluginsDir,
+		__DIR__ . $config->application->libraryDir,
+		__DIR__ . $config->application->modelsDir,
+		__DIR__ . $config->application->formsDir,
+	)
+)->register();
+
+$elements = new Elements();
