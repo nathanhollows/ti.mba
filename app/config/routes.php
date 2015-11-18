efa2a86b22b7a4842672194a98fd0b33148deb43
wisend <me@nathanhollows.com>
2015-11-18 13:16:26 +1300

    Default Namespace
    Set default namespace, removed redundant namespace lines

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,15 +2,15 @@
 
 $router = new Phalcon\Mvc\Router(false);
 
-// Define defaukt routes. File gets included in services.php
+$router->setDefaultNamespace('App\Controllers');
+
+// Define default routes. File gets included in services.php
 
 $router->add('/', array(
-	'namespace' => 'App\Controllers',
 	'controller' => 1
 ));
 
 $router->add('/:controller/:action/:params', array(
-	'namespace' => 'App\Controllers',
 	'controller' => 1,
 	'action' => 2,
 	'params' => 3,
@@ -21,49 +21,34 @@ $router->add('/:controller', array(
 	'controller' => 1
 ));
 
-$router->add('/admin/:controller/:action/:params', array(
-	'namespace' => 'App\Controllers\Admin',
-	'controller' => 1,
-	'action' => 2,
-	'params' => 3,
-));
-
-$router->add('/admin/:controller', array(
-	'namespace' => 'App\Controllers\Admin',
-	'controller' => 1
-));
-
-$router->add('/admin', array(
-	'namespace' => 'App\Controllers\Admin',
-	'controller' => 'index',
-	'action'	=>	'index'
-));
-
 // Auth controllers
 
 $router->add('/login', array(
-	'namespace' => 'App\Controllers',
 	'controller' => 'session',
 	'action'	=>	'login'
 ));
 
 $router->add('/logout', array(
-	'namespace' => 'App\Controllers',
 	'controller' => 'session',
 	'action'	=>	'logout'
 ));
 
 $router->add('/signup', array(
-	'namespace' => 'App\Controllers',
 	'controller' => 'session',
 	'action'	=>	'signup'
 ));
 
 $router->add('/forgotpassword', array(
-	'namespace' => 'App\Controllers',
 	'controller' => 'session',
 	'action'	=>	'forgotpassword'
 ));
+
+$router->notFound(
+  array(
+    'controller' => 'error',
+    'action' => 'show404'
+  )
+);
 
 // Remove trailing slahes automatically
 $router->removeExtraSlashes(true);
