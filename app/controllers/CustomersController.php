c448f45ee647df1bb16f1e3192e451f00aa8ee36
wisend <me@nathanhollows.com>
2015-11-20 09:40:55 +1300

    Customers
    Define controllers actions

--- app/controllers/CustomersController.php
+++ app/controllers/CustomersController.php
@@ -14,30 +14,50 @@ class CustomersController extends ControllerBase
 		parent::initialize();
 	}
 
+	// The start action, this shows the default 'search' view
 	public function indexAction()
 	{
-		$this->persistent->conditions = null;
-		$this->view->form = new CustomersForm();
 		$this->tag->prependTitle('Customers');
+		$this->persistent->searchParams	= null;
+		$this->view->form 				= new CustomersForm;
 	}
 
+	// Executes the search based on the criteria sent from the index
+	// Returning a paginator for the results
 	public function searchAction()
 	{
 
 	}
 
+	// Shows the view the create a new product
+	public function newAction()
+	{
+		$this->tag->prependTitle('New Customer');
+
+	}
+
+	// Creates a cusomer based on the data entered into the new action
 	public function createAction()
 	{
 
 	}
 
+	// Shows the view to edit an existing customer
+	public function editAction()
+	{
+
+	}
+
+	// Updates a customer based on the data entered in the edit action
+	public function saveAction()
+	{
+
+	}
+
+	// Deletes and existing customer
 	public function deleteAction()
 	{
 
 	}
 
-	public function viewAction()
-	{
-
-	}
 }
