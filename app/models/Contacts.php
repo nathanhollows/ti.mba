4a0928be3fd28d1cac168ff435ee2f4bbc702c06
wisend <nathanhollows@gmail.com>
2015-12-11 20:20:56 +1300

    Contacts Model

---
+++ app/models/Contacts.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Models;
+
+class Contacts extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $customerCode;
+
+    /**
+     *
+     * @var string
+     */
+    public $firstName;
+
+    /**
+     *
+     * @var string
+     */
+    public $lastName;
+
+    /**
+     *
+     * @var string
+     */
+    public $email;
+
+    /**
+     *
+     * @var string
+     */
+    public $directDial;
+
+    /**
+     *
+     * @var string
+     */
+    public $cellPhone;
+
+    /**
+     *
+     * @var string
+     */
+    public $position;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->belongsTo('customerCode', 'App\Models\Customers', 'customerCode');
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Contacts[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Contacts
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'contacts';
+    }
+
+}
