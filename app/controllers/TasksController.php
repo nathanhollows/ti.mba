b8c0bdeacfe3c65427f900901dd8796170fc7369
wisend <nathanhollows@gmail.com>
2015-12-16 20:30:59 +1300

    Tasks Controller

---
+++ app/controllers/TasksController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class TasksController extends ControllerBase
+{
+	public function initialize()
+	{
+		$this->view->setTemplateBefore('private');
+		parent::initialize();
+	}
+
+	public function indexAction()
+	{
+		
+	}
+}
