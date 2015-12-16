d55e747abb9d7f0b72165a97aa15c2e4c3a600d3
wisend <nathanhollows@gmail.com>
2015-12-16 20:30:44 +1300

    Quotes Controller

---
+++ app/controllers/QuotesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class QuotesController extends ControllerBase
+{
+	public function initialize()
+	{
+		$this->view->setTemplateBefore('private');
+		parent::initialize();
+	}
+
+	public function indexAction()
+	{
+		
+	}
+}
