6d6bfa043106a1991c8bdad4c2bfaee6287b96e4
wisend <nathanhollows@gmail.com>
2015-12-21 18:46:50 +1300

    ContactRecord model

---
+++ app/models/ContactRecord.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Models;
+
+class ContactRecord extends \Phalcon\Mvc\Model
+{
+	/**
+     *
+     * @var integer
+     */
+	public $id;
+
+    /**
+     *
+     * @var date
+     */
+	public $date;
+
+    /**
+     *
+     * @var string
+     */
+	public $customerCode;
+
+    /**
+     *
+     * @var integer
+     */
+	public $user;
+
+	/**
+     *
+     * @var integer
+     */
+	public $contactType;
+
+	/**
+     *
+     * @var integer
+     */
+	public $status;
+
+    /**
+     *
+     * @var string
+     */
+	public $contactReference;
+
+    /**
+     *
+     * @var string
+     */
+	public $details;
+
+	/**
+     *
+     * @var integer
+     */
+	public $followUpUser;
+
+    /**
+     *
+     * @var date
+     */
+	public $followUpDate;
+
+    /**
+     *
+     * @var string
+     */
+	public $followUpNotes;
+
+    /**
+     *
+     * @var string
+     */
+	public $infoSent;
+
+	/**
+     *
+     * @var integer
+     */
+	public $attachments;
+
+	/**
+     *
+     * @var integer
+     */
+	public $completed;
+
+	public function initialize()
+	{
+		$this->hasOne('user', 'App\Models\Users', 'id', array('alias'  => 'staff'));
+		$this->hasOne('contactType', 'App\Models\ContactType', 'id', array('alias'  => 'type'));
+	}
+}
