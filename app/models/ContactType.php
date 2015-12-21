055a8f7c4a9f58f2f1044948d230dd1caa51cd47
wisend <nathanhollows@gmail.com>
2015-12-21 18:46:57 +1300

    ContactType Model

---
+++ app/models/ContactType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+class ContactType extends \Phalcon\Mvc\Model
+{
+    /**
+     *
+     * @var integer
+     */
+	public $id;
+
+    /**
+     *
+     * @var string
+     */
+	public $name;
+}
