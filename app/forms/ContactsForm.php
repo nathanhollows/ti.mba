cc2ec9ac72b50a557526345b2cb8d9eb8e9d374f
wisend <nathanhollows@gmail.com>
2015-12-22 20:32:12 +1300

    New Contacts Form
    Not to be confused with messaging contact form

---
+++ app/forms/ContactsForm.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Forms;
+
+use Phalcon\Forms\Form;
+use Phalcon\Forms\Element\Text;
+use Phalcon\Forms\Element\Email;
+use Phalcon\Validation\Validator\PresenceOf;
+
+class ContactsForm extends Form
+{
+	/**
+	 * Initialize the products form
+	 */
+
+	public function initialize()
+	{
+		$name = new Text("name");
+		$name->setLabel("Name");
+		$name->setFilters(array('striptags', 'string'));
+		$name->addValidators(array(
+			new PresenceOf(array(
+				'message'	=> 'Name is required'
+			))
+		));
+		$name->setAttributes(array(
+			'class'			=> 'form-control',
+			'placeholder'	=> 'Name',
+			'required'		=> 'true'
+		));
+		$this->add($name);
+
+		$position = new Text("position");
+		$position->setLabel("Position");
+		$position->setAttributes(array(
+			'class'			=> 'form-control',
+			'placeholder'	=> 'Position',
+		));
+		$this->add($position);
+
+		$email = new Email("email");
+		$email->setLabel("Email Address");
+		$email->setAttributes(array(
+			'class'			=> 'form-control',
+			'placeholder'	=> 'email@example.com',
+		));
+		$this->add($email);
+
+		$phone = new Text("phone");
+		$phone->setLabel("Phone");
+		$phone->setAttributes(array(
+			'class'			=> 'form-control',
+			'placeholder'	=> 'Direct Dial',
+		));
+		$this->add($phone);	
+
+		$mobile = new Text("mobile");
+		$mobile->setLabel("Cell Phone");
+		$mobile->setAttributes(array(
+			'class'			=> 'form-control',
+			'placeholder'	=> 'Cell Phone',
+		));
+		$this->add($mobile);
+	}
+}
