b760ec82434ac229d88389999f2de0946eed7293
wisend <nathanhollows@gmail.com>
2015-12-22 20:32:28 +1300

    New Contacts groundwork in controller

--- app/controllers/ContactsController.php
+++ app/controllers/ContactsController.php
@@ -3,8 +3,9 @@
 namespace App\Controllers;
 
 use Phalcon\Mvc\Model\Criteria;
-use Phalcon\Mvc\Form;
+use Phalcon\Mvc\Forms;
 use App\Models\Contacts;
+use App\Forms\ContactsForm;
 
 class ContactsController extends ControllerBase
 {
@@ -25,5 +26,24 @@ class ContactsController extends ControllerBase
 		$this->view->contactDetails = $contactDetails;
 		$this->view->pageTitle = $contactDetails->name;
 		$this->view->pageSubtitle = $contactDetails->customers->customerName;
+	}
+
+	public function newAction()
+	{
+		$this->view->setTemplateBefore('blank');
+		$this->view->pageTitle = "Create new Contact";
+		$this->view->form = new ContactsForm;
+	}
+
+	public function createAction()
+	{
+        if (!$this->request->isPost()) {
+            return $this->dispatcher->forward(array(
+                "controller" => "customers",
+                "action" => "index"
+            ));
+        }
+
+        $contact = new Contacts();
 	}
 }
