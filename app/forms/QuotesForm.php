6e5131302cd9c0a61f77f7293f099a2b5da573cc
wisend <nathanhollows@gmail.com>
2015-12-23 20:21:48 +1300

    Quotes form groundwork

--- app/forms/QuotesForm.php
+++ app/forms/QuotesForm.php
@@ -4,6 +4,11 @@ namespace App\Forms;
 
 use Phalcon\Forms\Form;
 use Phalcon\Forms\Element\Text;
+use Phalcon\Forms\Element\Numeric;
+use Phalcon\Forms\Element\Select;
+use Phalcon\Validation\Validator\PresenceOf;
+
+use App\Models\Users;
 
 class QuotesForm extends Form
 {
@@ -12,9 +17,25 @@ class QuotesForm extends Form
 	 * Initiliaze the quotes form
 	 */
 
-	public function initialize($customerCode = null)
+	public function initialize()
 	{
 		$customerCode = new Text("customerCode");
+		$customerCode->setLabel("Customer");
+		$customerCode->setDefault($customer->customerName);
 		$this->add($customerCode);
+
+		$salesAgent = new Select(
+			"salesagent",
+			Users::find(),
+			array(
+				'using'	=> array(
+					'id',
+					'name'
+				)
+			)
+		);
+		$salesAgent->setLabel("Sales Agent");	
+		$salesAgent->setDefault('3');
+		$this->add($salesAgent);
 	}
 }
