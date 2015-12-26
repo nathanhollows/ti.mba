36368dad989b3127d65b80d3f7649f1c253a2bfc
wisend <nathanhollows@gmail.com>
2015-12-26 14:45:03 +1300

    Avaunt Schema

---
+++ schemas/avaunt.sql
@@ -0,0 +1,677 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 26, 2015 at 02:44 AM
+-- Server version: 10.1.8-MariaDB
+-- PHP Version: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `avaunt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addresses`
+--
+
+CREATE TABLE `addresses` (
+  `id` int(11) NOT NULL,
+  `customerCode` varchar(6) DEFAULT NULL,
+  `line1` varchar(255) DEFAULT NULL,
+  `line2` varchar(255) DEFAULT NULL,
+  `line3` varchar(255) DEFAULT NULL,
+  `suburb` varchar(255) DEFAULT NULL,
+  `zipCode` varchar(255) DEFAULT NULL,
+  `city` text,
+  `country` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address_types`
+--
+
+CREATE TABLE `address_types` (
+  `typeCode` int(11) NOT NULL,
+  `typeDescription` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `id` int(11) NOT NULL,
+  `customerCode` varchar(13) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(28) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(68) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `directDial` varchar(35) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `position` varchar(59) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_record`
+--
+
+CREATE TABLE `contact_record` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `date` date NOT NULL,
+  `customerCode` varchar(6) COLLATE utf8_unicode_ci NOT NULL,
+  `user` int(11) NOT NULL,
+  `contactType` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `contactReference` text COLLATE utf8_unicode_ci NOT NULL,
+  `details` text COLLATE utf8_unicode_ci NOT NULL,
+  `followUpUser` int(11) NOT NULL,
+  `followUpNotes` text COLLATE utf8_unicode_ci NOT NULL,
+  `infoSent` text COLLATE utf8_unicode_ci NOT NULL,
+  `attachments` int(11) NOT NULL,
+  `completed` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_type`
+--
+
+CREATE TABLE `contact_type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customerCode` varchar(6) COLLATE utf8_unicode_ci NOT NULL,
+  `customerName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `customerPhone` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `customerFax` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `customerEmail` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `freightArea` int(11) DEFAULT NULL,
+  `freightCarrier` int(11) DEFAULT NULL,
+  `salesArea` int(11) DEFAULT NULL,
+  `customerStatus` int(11) DEFAULT NULL,
+  `defaultAddress` int(11) DEFAULT NULL,
+  `billingAddress` int(11) DEFAULT NULL,
+  `defaultContact` int(11) DEFAULT NULL,
+  `customerGroup` int(11) DEFAULT NULL,
+  `branchCode` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_addresses`
+--
+
+CREATE TABLE `customer_addresses` (
+  `customerAddressId` int(11) NOT NULL,
+  `addressId` int(11) NOT NULL,
+  `typeCode` int(11) NOT NULL,
+  `customerCode` varchar(6) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_groups`
+--
+
+CREATE TABLE `customer_groups` (
+  `id` int(11) NOT NULL,
+  `name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_status`
+--
+
+CREATE TABLE `customer_status` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `style` text NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `warning` tinyint(1) NOT NULL,
+  `statusNote` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dryness`
+--
+
+CREATE TABLE `dryness` (
+  `id` int(11) NOT NULL,
+  `shortCode` varchar(10) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `email_confirmations`
+--
+
+CREATE TABLE `email_confirmations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `usersId` int(10) UNSIGNED NOT NULL,
+  `code` char(32) NOT NULL,
+  `createdAt` int(10) UNSIGNED NOT NULL,
+  `modifiedAt` int(10) UNSIGNED DEFAULT NULL,
+  `confirmed` char(1) DEFAULT 'N'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_logins`
+--
+
+CREATE TABLE `failed_logins` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `usersId` int(10) UNSIGNED DEFAULT NULL,
+  `ipAddress` char(15) NOT NULL,
+  `attempted` smallint(5) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `finish`
+--
+
+CREATE TABLE `finish` (
+  `id` int(11) NOT NULL,
+  `shortCode` varchar(10) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `freight_areas`
+--
+
+CREATE TABLE `freight_areas` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `freight_carriers`
+--
+
+CREATE TABLE `freight_carriers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grade`
+--
+
+CREATE TABLE `grade` (
+  `id` int(11) NOT NULL,
+  `shortCode` varchar(10) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_changes`
+--
+
+CREATE TABLE `password_changes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `usersId` int(10) UNSIGNED NOT NULL,
+  `ipAddress` char(15) NOT NULL,
+  `userAgent` varchar(48) NOT NULL,
+  `createdAt` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pbt_consignments`
+--
+
+CREATE TABLE `pbt_consignments` (
+  `customerConsignment` text COLLATE utf8_unicode_ci,
+  `pbtConsignmentNote` text COLLATE utf8_unicode_ci,
+  `numberOfItems` int(11) DEFAULT NULL,
+  `weight` float DEFAULT NULL,
+  `pickupDate` date DEFAULT NULL,
+  `podDate` date DEFAULT NULL,
+  `podTime` time DEFAULT NULL,
+  `deliveryBy` text COLLATE utf8_unicode_ci,
+  `podSignature` text COLLATE utf8_unicode_ci,
+  `deliveryCourier` text COLLATE utf8_unicode_ci,
+  `ticketNo` text COLLATE utf8_unicode_ci,
+  `cost` float DEFAULT NULL,
+  `runsheet` text COLLATE utf8_unicode_ci,
+  `accountNo` int(11) DEFAULT NULL,
+  `volume` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `profilesId` int(10) UNSIGNED NOT NULL,
+  `resource` varchar(16) NOT NULL,
+  `action` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `active` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `remember_tokens`
+--
+
+CREATE TABLE `remember_tokens` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `usersId` int(10) UNSIGNED NOT NULL,
+  `token` char(32) NOT NULL,
+  `userAgent` varchar(120) NOT NULL,
+  `createdAt` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reset_passwords`
+--
+
+CREATE TABLE `reset_passwords` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `usersId` int(10) UNSIGNED NOT NULL,
+  `code` varchar(48) NOT NULL,
+  `createdAt` int(10) UNSIGNED NOT NULL,
+  `modifiedAt` int(10) UNSIGNED DEFAULT NULL,
+  `reset` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_areas`
+--
+
+CREATE TABLE `sales_areas` (
+  `id` int(11) NOT NULL,
+  `name` varchar(25) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `success_logins`
+--
+
+CREATE TABLE `success_logins` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `usersId` int(10) UNSIGNED NOT NULL,
+  `ipAddress` char(15) NOT NULL,
+  `userAgent` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `treatment`
+--
+
+CREATE TABLE `treatment` (
+  `id` int(11) NOT NULL,
+  `shortCode` varchar(10) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `position` varchar(50) DEFAULT NULL,
+  `directDial` varchar(50) DEFAULT NULL,
+  `password` char(60) NOT NULL,
+  `mustChangePassword` char(1) DEFAULT NULL,
+  `profilesId` int(10) UNSIGNED DEFAULT NULL,
+  `banned` char(1) DEFAULT NULL,
+  `suspended` char(1) DEFAULT NULL,
+  `active` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `addresses`
+--
+ALTER TABLE `addresses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `address_types`
+--
+ALTER TABLE `address_types`
+  ADD PRIMARY KEY (`typeCode`);
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact_record`
+--
+ALTER TABLE `contact_record`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customerCode` (`customerCode`);
+
+--
+-- Indexes for table `contact_type`
+--
+ALTER TABLE `contact_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customerCode`),
+  ADD UNIQUE KEY `customerCode` (`customerCode`),
+  ADD KEY `customerGroup` (`defaultAddress`),
+  ADD KEY `freightArea` (`freightArea`,`freightCarrier`,`salesArea`,`customerStatus`,`defaultAddress`,`defaultContact`),
+  ADD KEY `customerStatus` (`customerStatus`),
+  ADD KEY `freightCarrier` (`freightCarrier`),
+  ADD KEY `salesArea` (`salesArea`),
+  ADD KEY `customerGroup_2` (`customerGroup`);
+
+--
+-- Indexes for table `customer_addresses`
+--
+ALTER TABLE `customer_addresses`
+  ADD PRIMARY KEY (`customerAddressId`),
+  ADD UNIQUE KEY `addressId` (`customerAddressId`),
+  ADD KEY `addressId_2` (`addressId`,`typeCode`,`customerCode`),
+  ADD KEY `typeCode` (`typeCode`),
+  ADD KEY `customerCode` (`customerCode`);
+
+--
+-- Indexes for table `customer_groups`
+--
+ALTER TABLE `customer_groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customer_status`
+--
+ALTER TABLE `customer_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dryness`
+--
+ALTER TABLE `dryness`
+  ADD UNIQUE KEY `shortCode` (`shortCode`);
+
+--
+-- Indexes for table `email_confirmations`
+--
+ALTER TABLE `email_confirmations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_logins`
+--
+ALTER TABLE `failed_logins`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usersId` (`usersId`);
+
+--
+-- Indexes for table `finish`
+--
+ALTER TABLE `finish`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `shortCode` (`shortCode`);
+
+--
+-- Indexes for table `freight_areas`
+--
+ALTER TABLE `freight_areas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `freight_carriers`
+--
+ALTER TABLE `freight_carriers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `grade`
+--
+ALTER TABLE `grade`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `shortCode` (`shortCode`);
+
+--
+-- Indexes for table `password_changes`
+--
+ALTER TABLE `password_changes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usersId` (`usersId`);
+
+--
+-- Indexes for table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `profilesId` (`profilesId`);
+
+--
+-- Indexes for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `active` (`active`);
+
+--
+-- Indexes for table `remember_tokens`
+--
+ALTER TABLE `remember_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `token` (`token`);
+
+--
+-- Indexes for table `reset_passwords`
+--
+ALTER TABLE `reset_passwords`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usersId` (`usersId`);
+
+--
+-- Indexes for table `sales_areas`
+--
+ALTER TABLE `sales_areas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `success_logins`
+--
+ALTER TABLE `success_logins`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usersId` (`usersId`);
+
+--
+-- Indexes for table `treatment`
+--
+ALTER TABLE `treatment`
+  ADD UNIQUE KEY `shortCode` (`shortCode`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `profilesId` (`profilesId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `addresses`
+--
+ALTER TABLE `addresses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2896;
+--
+-- AUTO_INCREMENT for table `address_types`
+--
+ALTER TABLE `address_types`
+  MODIFY `typeCode` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `customer_addresses`
+--
+ALTER TABLE `customer_addresses`
+  MODIFY `customerAddressId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2896;
+--
+-- AUTO_INCREMENT for table `customer_groups`
+--
+ALTER TABLE `customer_groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `customer_status`
+--
+ALTER TABLE `customer_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `email_confirmations`
+--
+ALTER TABLE `email_confirmations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `failed_logins`
+--
+ALTER TABLE `failed_logins`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT for table `finish`
+--
+ALTER TABLE `finish`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `freight_areas`
+--
+ALTER TABLE `freight_areas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `freight_carriers`
+--
+ALTER TABLE `freight_carriers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `grade`
+--
+ALTER TABLE `grade`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `password_changes`
+--
+ALTER TABLE `password_changes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+--
+-- AUTO_INCREMENT for table `profiles`
+--
+ALTER TABLE `profiles`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `remember_tokens`
+--
+ALTER TABLE `remember_tokens`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `reset_passwords`
+--
+ALTER TABLE `reset_passwords`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sales_areas`
+--
+ALTER TABLE `sales_areas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `success_logins`
+--
+ALTER TABLE `success_logins`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
