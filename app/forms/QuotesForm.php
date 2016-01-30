8808d7f9991bee7e3589de960344890465b22a01
wisend <nathanhollows@gmail.com>
2016-01-30 20:46:30 +1300

    Adds many elements to quotes form

--- app/forms/QuotesForm.php
+++ app/forms/QuotesForm.php
@@ -4,8 +4,12 @@ namespace App\Forms;
 
 use Phalcon\Forms\Form;
 use Phalcon\Forms\Element\Text;
+use Phalcon\Forms\Element\Date;
+use Phalcon\Forms\Element\TextArea;
 use Phalcon\Forms\Element\Numeric;
 use Phalcon\Forms\Element\Select;
+use Phalcon\Forms\Element\Submit;
+use Phalcon\Validation;
 use Phalcon\Validation\Validator\PresenceOf;
 
 use App\Models\Users;
@@ -19,9 +23,55 @@ class QuotesForm extends Form
 
 	public function initialize()
 	{
+		$validation = new Validation();
+
 		$customerCode = new Text("customerCode");
 		$customerCode->setLabel("Customer");
+		$customerCode->setAttributes(array(
+			'required'	=> 'true',
+			'class'		=> 'form-control'
+		));
 		$this->add($customerCode);
+
+		$contact = new Text("contact");
+		$contact->setAttributes(array(
+			'class'		=> 'form-control'
+		));
+		$contact->setLabel("Contact");
+		$this->add($contact);
+
+		$validation->add(
+		    $contact,
+		    new PresenceOf(
+		        array(
+		            'message' => 'A contact is required'
+		        )
+		    )
+		);
+
+		$customerRef = new Text("customerRef");
+		$customerRef->setAttributes(array(
+			'required'	=> 'true',
+			'class'		=> 'form-control'
+		));
+		$customerRef->setLabel("Customer Reference");
+		$this->add($customerRef);
+
+		$date = new Date("date");
+		$date->setAttributes(array(
+			'required'	=> 'true',
+			'class'		=> 'form-control'
+		));
+		$date->setLabel("Date");
+		$date->setDefault(date('Y-m-d'));
+		$this->add($date);
+
+		$notes = new TextArea("notes");
+		$notes->setAttributes(array(
+			'class'		=> 'form-control'
+		));
+		$notes->setLabel("Notes");
+		$this->add($notes);
 
 		$salesAgent = new Select(
 			"salesagent",
@@ -30,11 +80,18 @@ class QuotesForm extends Form
 				'using'	=> array(
 					'id',
 					'name'
-				)
+				),
+				'class'	=> 'form-control'
 			)
 		);
 		$salesAgent->setLabel("Sales Agent");	
 		$salesAgent->setDefault('3');
 		$this->add($salesAgent);
+
+		$submit = new Submit("submit");
+		$submit->setAttributes(array(
+			'class'	=> 'btn btn-primary'
+		));
+		$this->add($submit);
 	}
 }
