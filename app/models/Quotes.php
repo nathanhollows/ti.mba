34839729194bc19f7d8ba201541f42ee74351125
wisend <nathanhollows@gmail.com>
2016-02-03 20:30:52 +1300

    Relate quotes to users

--- app/models/Quotes.php
+++ app/models/Quotes.php
@@ -24,5 +24,6 @@ class Quotes extends Model
 	{
 		$this->hasOne('customerCode', 'App\Models\Customers', 'customerCode', array('alias' => 'customer'));
 		$this->hasOne('contact', 'App\Models\Contacts', 'id', array('alias' => 'customerContact'));
+		$this->hasOne('user', 'App\Models\Users', 'id', array('alias' => 'salesRep'));
 	}
 }
