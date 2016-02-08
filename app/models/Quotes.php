83a97460cba1db4a2198c0bcf2ae9901aeb96a94
wisend <nathanhollows@gmail.com>
2016-02-08 19:44:40 +1300

    Add Status and Details to quote model

--- app/models/Quotes.php
+++ app/models/Quotes.php
@@ -16,14 +16,19 @@ class Quotes extends Model
 
 	public $customerRef;
 
+	public $details;
+
 	public $user;
 
 	public $contact;
+
+	public $status;
 
 	public function initialize()
 	{
 		$this->hasOne('customerCode', 'App\Models\Customers', 'customerCode', array('alias' => 'customer'));
 		$this->hasOne('contact', 'App\Models\Contacts', 'id', array('alias' => 'customerContact'));
 		$this->hasOne('user', 'App\Models\Users', 'id', array('alias' => 'salesRep'));
+		$this->hasOne('status', 'App\Models\GenericStatus', 'id', array('alias' => 'genericStatus'));
 	}
 }
