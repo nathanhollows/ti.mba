c43f08bc53e3b7e131cebef0134a351e618517e7
wisend <nathanhollows@gmail.com>
2016-02-25 20:10:36 +1300

    Created address form

---
+++ app/forms/AddressForm.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Forms;
+
+use Phalcon\Forms\Form,
+	Phalcon\Forms\Element\Select,
+	Phalcon\Forms\Element\Text;
+
+use App\Models\AddressTypes,
+	App\Models\Customers;
+
+class AddressForm extends Form
+{
+	/**
+	 * Initialize the address form
+	 */
+
+	public function initialize($entity = null, $option = null)
+	{
+
+		$customerCode = new Select(
+			'customerCode',
+			Customers::find(),
+			array(
+				'using' => array('customerCode', 'customerName'),
+				'required'	=> 'true',
+				'useEmpty'	=> true,
+				'class' => 'form-control select2'
+			)
+		);
+		$customerCode->setLabel("Customer");
+		$this->add($customerCode);
+
+		$type = new Select(
+			'typeCode',
+			AddressTypes::find(),
+			array(
+				'using' => array('typeCode', 'typeDescription'),
+				'required'	=> 'true',
+				'class' => 'form-control'
+			)
+		);
+		$type->setLabel("Type");
+		$this->add($type);
+
+		$line1 = new Text('line1');
+		$line1->setLabel('Line 1');
+		$line1->setAttributes(array(
+			"class"	=> "form-control",
+		));
+		$this->add($line1);
+
+		$line2 = new Text('line2');
+		$line2->setLabel('Line 2');
+		$line2->setAttributes(array(
+			"class"	=> "form-control",
+		));
+		$this->add($line2);
+
+		$line3 = new Text('line3');
+		$line3->setLabel('Line 3');
+		$line3->setAttributes(array(
+			"class"	=> "form-control",
+		));
+		$this->add($line3);
+
+		$zip = new Text('zipCode');
+		$zip->setLabel('Zip Code');
+		$zip->setAttributes(array(
+			"class"	=> "form-control",
+		));
+		$this->add($zip);
+
+		$city = new Text('city');
+		$city->setLabel('City / Town');
+		$city->setAttributes(array(
+			"class"	=> "form-control",
+		));
+		$this->add($city);
+
+	}
+}
