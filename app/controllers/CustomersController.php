53273a4196b2053d3b87685d96e2f67aeaae6ae9
wisend <nathanhollows@gmail.com>
2016-02-25 20:45:56 +1300

    :lipstick:

--- app/controllers/CustomersController.php
+++ app/controllers/CustomersController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controllers;
- 
+
 use DataTables\DataTable;
 use Phalcon\Mvc\Model\Criteria;
 use Phalcon\Mvc\Model;
@@ -39,8 +39,8 @@ class CustomersController extends ControllerBase
             $dataTables = new DataTable();
             $dataTables->fromBuilder($builder)->sendResponse();
             $this->persistent->parameters = null;
-      };
-  }
+        };
+    }
 
     /**
      * Displays the creation form
@@ -60,58 +60,71 @@ class CustomersController extends ControllerBase
     public function viewAction($customerCode)
     {
 
-        if (!$this->request->isPost()) {
+        $this->view->parser = new \cebe\markdown\Markdown();
+        $customer = Customers::findFirstBycustomerCode($customerCode);
+        if (!$customer) {
+            $this->flash->error("Customer was not found");
 
-            $customer = Customers::findFirstBycustomerCode($customerCode);
-            if (!$customer) {
-                $this->flash->error("Customer was not found");
-
-                return $this->dispatcher->forward(array(
-                    "controller" => "customers",
-                    "action" => "index"
+            return $this->dispatcher->forward(array(
+                "controller" => "customers",
+                "action" => "index"
                 ));
-            }
-
-            $quotes = Quotes::find(array(
-                "customerCode = '$customerCode'",
-                'order'         => 'id DESC'));
-            $this->view->quotes = $quotes;
-
-            $history = ContactRecord::find(array(
-                "customerCode = '$customerCode'",
-                'order'         => 'date DESC',
-                'limit'         => 8
-            ));
-
-            $notes = CustomerNotes::find(array(
-                "customerCode = '$customerCode'",
-                'order'         => 'date DESC',
-            ));
-
-            $this->view->customerCode = $customer->customerCode;
-            $this->tag->setDefault("customerCode", $customer->customerCode);
-            $this->tag->setDefault("customerName", $customer->customerName);
-            $this->tag->setDefault("customerPhone", $customer->customerPhone);
-            $this->tag->setDefault("customerFax", $customer->customerFax);
-            $this->tag->setDefault("customerEmail", $customer->customerEmail);
-            $this->tag->setDefault("freightArea", $customer->freightArea);
-            $this->tag->setDefault("freightCarrier", $customer->freightCarrier);
-            $this->tag->setDefault("salesArea", $customer->salesArea);
-            $this->tag->setDefault("customerStatus", $customer->customerStatus);
-            $this->tag->setDefault("defaultAddress", $customer->defaultAddress);
-            $this->tag->setDefault("defaultContact", $customer->defaultContact);
-            $this->tag->setDefault("customerGroup", $customer->customerGroup);
-            
-            $this->view->customer = $customer;
-            $this->view->history = $history;
-            $this->view->notes = $notes;
-
-            $addresses = CustomerAddresses::find("customerCode = '$customerCode'");
-            $this->view->addresses = $addresses;
-            $this->view->pageTitle = $customer->customerCode;
-            $this->view->pageSubtitle = $customer->customerName;
-            $this->tag->prependTitle($customer->customerName);
         }
+
+        $quotes = Quotes::find(array(
+            "customerCode = '$customerCode'",
+            'order'         => 'id DESC'));
+        $this->view->quotes = $quotes;
+
+        $history = ContactRecord::find(array(
+            "customerCode = '$customerCode'",
+            'order'         => 'date DESC',
+            'limit'         => 8
+            ));
+        $this->view->history = $history;
+
+        $notes = CustomerNotes::find(array(
+            "customerCode = '$customerCode'",
+            'order'         => 'date DESC',
+            ));
+
+        $this->view->customerCode = $customer->customerCode;
+        $this->tag->setDefault("customerCode", $customer->customerCode);
+        $this->tag->setDefault("customerName", $customer->customerName);
+        $this->tag->setDefault("customerPhone", $customer->customerPhone);
+        $this->tag->setDefault("customerFax", $customer->customerFax);
+        $this->tag->setDefault("customerEmail", $customer->customerEmail);
+        $this->tag->setDefault("freightArea", $customer->freightArea);
+        $this->tag->setDefault("freightCarrier", $customer->freightCarrier);
+        $this->tag->setDefault("salesArea", $customer->salesArea);
+        $this->tag->setDefault("customerStatus", $customer->customerStatus);
+        $this->tag->setDefault("defaultAddress", $customer->defaultAddress);
+        $this->tag->setDefault("defaultContact", $customer->defaultContact);
+        $this->tag->setDefault("customerGroup", $customer->customerGroup);
+
+        $this->view->customer = $customer;
+        $this->view->notes = $notes;
+        $this->view->headerButton = \Phalcon\Tag::linkTo(array("followup/?company=" . $customerCode, '<i class="fa fa-plus"></i> Add Record', "class" => "btn btn-default pull-right", "data-target" => "#modal-ajax"));
+
+        $addresses = Addresses::find("customerCode = '$customerCode'");
+        $this->view->addresses = $addresses;
+        
+        $this->view->pageTitle = $customer->customerCode;
+        $this->view->pageSubtitle = $customer->customerName;
+        $this->tag->prependTitle($customer->customerName);
+
+    }
+
+    public function editAction($customerCode = null)
+    {
+        if ($this->request->isAjax()) {
+            $this->view->setTemplateBefore('modal-form');
+        }
+
+        $this->view->pageTitle = "Edit";
+
+        $customer = Customers::findFirstByCustomerCode($customerCode);
+        $this->view->form = new CustomersForm($customer);
 
     }
 
@@ -124,19 +137,42 @@ class CustomersController extends ControllerBase
             return $this->dispatcher->forward(array(
                 "controller" => "customers",
                 "action" => "index"
-            ));
+                ));
         }
 
         $customer = new Customers();
-        $success = $customer->save($this->request->getPost(), array('customerCode', 'customerCode', 'customerName', 'customerPhone', 'customerFax', 'customerEmail', 'freightArea', 'freightCarrier', 'salesArea', 'customerStatus', 'defaultAddress', 'defaultContact', 'customerGroup'));     
-        if ($success) {
-            $this->response->redirect("customers/view/" . $customer->customerCode);
-        } else {
+
+        $customer->customerCode = $this->request->getPost("customerCode");
+        $customer->customerName = $this->request->getPost("customerName");
+        $customer->customerPhone = $this->request->getPost("customerPhone");
+        $customer->customerFax = $this->request->getPost("customerFax");
+        $customer->customerEmail = $this->request->getPost("customerEmail");
+        $customer->freightArea = $this->request->getPost("freightArea");
+        $customer->freightCarrier = $this->request->getPost("freightCarrier");
+        $customer->salesArea = $this->request->getPost("salesArea");
+        $customer->customerStatus = $this->request->getPost("customerStatus");
+        $customer->defaultAddress = $this->request->getPost("defaultAddress");
+        $customer->defaultContact = $this->request->getPost("defaultContact");
+        $customer->customerGroup = $this->request->getPost("customerGroup");
+        
+
+        if (!$customer->save()) {
             foreach ($customer->getMessages() as $message) {
-                $this->flash->error($message->getMessage());
+                $this->flash->error($message);
             }
+
+            return $this->dispatcher->forward(array(
+                "controller" => "customers",
+                "action" => "new"
+                ));
         }
 
+        $this->flash->success("Customer was created successfully");
+
+        return $this->dispatcher->forward(array(
+            "controller" => "customers",
+            "action" => "view/" . $customer->customerCode,
+            ));
     }
 
     /**
@@ -152,7 +188,7 @@ class CustomersController extends ControllerBase
             return $this->dispatcher->forward(array(
                 "controller" => "customers",
                 "action" => "index"
-            ));
+                ));
         }
 
         $name = $this->request->getPost('name');
@@ -182,7 +218,7 @@ class CustomersController extends ControllerBase
             return $this->dispatcher->forward(array(
                 "controller" => "customers",
                 "action" => "index"
-            ));
+                ));
         }
 
         $customerCode = $this->request->getPost("customerCode");
@@ -194,7 +230,7 @@ class CustomersController extends ControllerBase
             return $this->dispatcher->forward(array(
                 "controller" => "customers",
                 "action" => "index"
-            ));
+                ));
         }
 
         $customer->customerCode = $this->request->getPost("customerCode");
@@ -221,7 +257,7 @@ class CustomersController extends ControllerBase
                 "controller" => "customers",
                 "action" => "edit",
                 "params" => array($customer->customerCode)
-            ));
+                ));
         }
 
         $this->flash->success("customer was updated successfully");
@@ -229,7 +265,7 @@ class CustomersController extends ControllerBase
         return $this->dispatcher->forward(array(
             "controller" => "customers",
             "action" => "index"
-        ));
+            ));
     }
 
     /**
@@ -246,7 +282,7 @@ class CustomersController extends ControllerBase
             return $this->dispatcher->forward(array(
                 "controller" => "customers",
                 "action" => "index"
-            ));
+                ));
         }
 
         if (!$customer->delete()) {
@@ -258,7 +294,7 @@ class CustomersController extends ControllerBase
             return $this->dispatcher->forward(array(
                 "controller" => "customers",
                 "action" => "search"
-            ));
+                ));
         }
 
         $this->flash->success("Customer was deleted successfully");
@@ -266,7 +302,7 @@ class CustomersController extends ControllerBase
         return $this->dispatcher->forward(array(
             "controller" => "customers",
             "action" => "index"
-        ));
+            ));
     }
 
 }
