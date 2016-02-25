508f0031b79ab811bd958528f748ff359c066768
wisend <nathanhollows@gmail.com>
2016-02-25 19:42:41 +1300

    CSS and JS for bootstrap markdown

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,48 @@
-$("a[data-target=#modal-ajax]").click(function(ev) {
-    ev.preventDefault();
-    var target = $(this).attr("href");
+$(document).ready(function() {
+	$('.dataTable').each(function () {
+		var source = $(this).attr("data-source");
+		var state = $(this).attr("data-state");
+		$(this).DataTable({
+			serverSide: true,
+			ajax: {
+				url: source,
+				method: 'POST'
+			},
+			search: {
+				smart: true
+			},
+			stateSave: true,
+			pagingType: "simple_numbers",
+			columns: [
+			{data: "customerCode", searchable: true,
+			"fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+				$(nTd).html("<a href='view/"+oData.customerCode+"'>"+oData.customerCode+"</a>");
+			}},
+			{data: "customerName", searchable: true},
+			{data: "customerPhone", searchable: true,
+			"fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+				$(nTd).html("<a href='tel:"+oData.customerPhone+"')>"+oData.customerPhone+"</a>");
+			}},
+			{data: "customerFax", searchable: false, class: "hidden-xs"},
+			{data: "name", searchable: true, class: "hidden-xs",
+			"fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+				$(nTd).html("<span class='label label-"+oData.style+"'>"+oData.name+"</span>");
+			}},
+			],
+		});
+	});
+	$('div.dataTables_filter input').select();
+	$("a[data-target=#modal-ajax]").click(function(ev) {
+		ev.preventDefault();
+		var target = $(this).attr("href");
 
     // load the url and show modal on success
+    $.fn.modal.Constructor.prototype.enforceFocus = function() {
+    	$( ".select2, .select2-multiple" ).select2( {  } );
+    	$("#details").markdown({ })
+    };
     $("#modal-ajax .modal-content").load(target, function() { 
-         $("#modal-ajax").modal("show"); 
+    	$("#modal-ajax").modal("show"); 
     });
+});
 });
