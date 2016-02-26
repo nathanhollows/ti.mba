3af56801ba02fefc1b9015bfbda99693803a373c
wisend <nathanhollows@gmail.com>
2016-02-26 23:28:25 +1300

    Show task count

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Freight\Freight;
+use App\Models\ContactRecord;
 
 class DashboardController extends ControllerBase
 {
@@ -17,6 +18,10 @@ class DashboardController extends ControllerBase
 
     public function indexAction()
     {
+        $tasks = new ContactRecord;
+
+        $this->view->tasks = $tasks->getTasks();
+
         $freight = new Freight;
         $this->view->pbt = $freight->downloadPBT();
         $this->view->pbt = $freight->importPBT();
