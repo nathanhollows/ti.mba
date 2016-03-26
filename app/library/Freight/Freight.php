88b6eead6bc4855941962d87fc64ddfd1e713a7a
wisend <nathanhollows@gmail.com>
2016-03-26 21:27:01 +1300

    If FTP login error then flash error

--- app/library/Freight/Freight.php
+++ app/library/Freight/Freight.php
@@ -21,6 +21,10 @@ class Freight extends component
 
 		// set up basic connection
 		$conn_id = ftp_connect($ftp_server); 
+		if (!$conn_id) {
+			$this->flash->error("The FTP connection to PBT has failed. Please check the config file and try again.");
+			return false;
+		}
 
 		// login with username and password
 		$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass); 
