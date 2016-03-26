1c7ef2f61c0bf6665675ca590b27b3c5f49ccc61
wisend <nathanhollows@gmail.com>
2016-03-26 21:13:24 +1300

    Calendar model and function

---
+++ app/models/Calendar.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Phalcon\Mvc\Model;
+
+class Calendar extends Model
+{
+	public $calendarDate;
+
+	public $day;
+
+	public $month;
+
+	public $year;
+
+	public $dayOfWeek;
+
+	public $dayOfMonth;
+
+	public $dayOfYear;
+
+	public $weekOfMonth;
+
+	public $weekday;
+
+	public $weekend;
+
+	public $wprkDay;
+
+	public $payday;
+
+	public $holiday;
+
+	public function nextWorkingDay()
+	{
+
+	}
+
+	public function previousWorkingDay()
+	{
+
+	}
+
+	public function isWeekend()
+	{
+		if ($this->weekend == 1){
+			return true;
+		}
+	}
+
+	public function isWorkDay()
+	{
+		if ($this->workDay == 1){
+			return true;
+		}
+	}
+}
