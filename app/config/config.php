115c5c72ae31b82485cac88f00157f0a1ad6f203
wisend <nathanhollows@gmail.com>
2016-03-26 21:05:19 +1300

    Change base URI

--- app/config/config.php
+++ app/config/config.php
@@ -20,7 +20,7 @@ return new \Phalcon\Config(array(
 		'pluginsDir'     => __DIR__ . '/../../app/plugins/',
 		'libraryDir'     => __DIR__ . '/../../app/library/',
 		'cacheDir'       => __DIR__ . '/../../app/cache/',
-		'baseUri'        => '/avaunt/',
+		'baseUri'        => '/',
 		// Set the root URI
 		// Define the site title
 		'siteTitle'		 => 'Avaunt',
