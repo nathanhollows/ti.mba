369e139d3de7d62b42d93c726a970f033a206056
wisend <nathanhollows@gmail.com>
2016-03-26 21:27:36 +1300

    KPI Controller

---
+++ app/controllers/KpiController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controllers;
+
+use Phalcon\Tag;
+
+use App\Forms\KpiForm,
+    App\Models\Kpis,
+    App\Models\Calendar;
+
+class KpiController extends ControllerBase
+{
+
+	public function initialize()
+	{
+        $this->view->setTemplateBefore('private');
+        parent::initialize();
+	}
+
+    public function indexAction()
+    {
+    	$this->tag->prependTitle('KPI');
+        $this->response->redirect('kpi/' . date('Y/m/d'));
+    }
+
+    public function editAction($year, $month, $day)
+    {
+        // Set the title
+    	$this->tag->prependTitle('KPI');
+        // Check if URL is correctly formed date
+        if (checkdate($month, $day, $year)) {
+            $dateRaw = strtotime("$year-$month-$day");
+            $date = date('Y/m/d', $dateRaw);
+        } else {
+            $this->flash->error("This is not a valid date");
+            return true;
+        }
+
+        $form = new KpiForm;
+        $data = Kpis::findFirst("date = '$date'");
+        if ($data) {
+            $form = new KpiForm($data);
+        }
+        $this->view->form = $form;
+
+        $this->view->tomorrow = date('Y/m/d', strtotime($date . "+1 days"));
+        $this->view->yesterday = date('Y/m/d', strtotime($date . "-1 days"));
+
+        $calendar = Calendar::findFirst("calendarDate = '$date'");
+
+        if (!$calendar->isWorkDay()) {
+            $this->flash->warning("This is not a working day");
+        }
+    }
+
+    public function saveAction()
+    {
+        echo "Hello, world!";
+    }
+
+    // Action to generate TV dashboard view
+    public function dashboardAction($year = null, $month = null, $day = null)
+    {
+        if (!$year || !$month || !$day) {
+            $this->response->redirect('kpi/dashboard/' . date('Y/m/d'));
+        }
+
+        $this->view->setTemplateBefore('blank');
+    }
+}
