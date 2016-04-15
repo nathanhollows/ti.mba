2784536653ab4f2d369f8082b6225ab307750cb6
wisend <nathanhollows@gmail.com>
2016-04-15 22:17:31 +1200

    Model for Pricing Methods

---
+++ app/models/PricingUnit.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Phalcon\Mvc\Model;
+
+class PricingUnit extends Model
+{
+	/**
+     *
+     * @var integer
+     */
+	public $id;
+
+	/**
+     *
+     * @var string
+     */
+	public $name;
+}
