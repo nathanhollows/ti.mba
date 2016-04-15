61443a9ae7648bebd9ddbb7105ac43cba3e5587e
wisend <nathanhollows@gmail.com>
2016-04-15 21:48:37 +1200

    UserController base

---
+++ app/controllers/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers;
+
+class UserController extends ControllerBase
+{
+
+	public function initialize()
+	{
+		$this->view->setTemplateBefore('private');
+		parent::initialize();
+	}
+
+	public function indexAction()
+	{
+		$this->tag->prependTitle('Users');
+	}
+
+	public function editAction()
+	{
+		$this->tag->prependTitle('Users');
+	}
+
+	public function newAction()
+	{
+		$this->tag->prependTitle('Users');
+	}
+
+	public function createAction()
+	{
+		$this->view->disable();
+	}
+
+	public function updateAction()
+	{
+		$this->view->disable();
+	}
+}
