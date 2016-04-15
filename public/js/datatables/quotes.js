69e36ac4fd29a11e8dfd932c5406fd097fdca4e9
wisend <nathanhollows@gmail.com>
2016-04-15 22:33:05 +1200

    Quotes Datatables JS

---
+++ public/js/datatables/quotes.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	$('.dataTable').each(function () {
+		var source = $(this).attr("data-source");
+		var state = $(this).attr("data-state");
+		$(this).DataTable({
+			serverSide: true,
+			ajax: {
+				url: source,
+				method: 'POST'
+			},
+			search: {
+				smart: true
+			},
+			saveState: true,
+			paging_type: "simple_numbers",
+			order: [0, 'desc'],
+			columns: [
+			{data: "quoteId",			
+			"fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+				$(nTd).html("<a href='view/"+oData.quoteId+"'>"+oData.quoteId+"</a>");
+			}},
+			{data: "date"},
+			{data: "customerCode"},
+			{data: "reference"},
+			{data: "user",
+			"fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+				$(nTd).html(""+oData.salesRep+"");
+			}},
+			{data: "attention"},
+			{data: "status", class: "hidden-xs",
+			"fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+				$(nTd).html("<span class='label label-"+oData.style+"'>"+oData.name+"</span>");
+			}}
+			]
+		});
+	});
+	$('div.dataTables_filter input').select();
+});
