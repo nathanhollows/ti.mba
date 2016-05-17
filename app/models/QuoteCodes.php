58acca003bcfb5304381ab6c1ded024819c9e104
Wisend <me@nathanhollows.com>
2016-05-17 20:03:06 +1200

    Created quote codes model

---
+++ app/models/QuoteCodes.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Phalcon\Mvc\Model;
+
+class QuoteCodes extends Model
+{
+	public $code;
+
+	public $description;
+
+	public $grade;
+
+	public $treatment;
+
+	public $dryness;
+
+	
+}
