b9e89e2735cbe1fc0616368663199c32c887f6e0
Wisend <me@nathanhollows.com>
2016-06-03 21:51:07 +1200

    AJAX testing, return posted data

---
+++ app/controllers/AjaxController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+class AjaxController extends ControllerBase
+{
+	public function initialize()
+	{
+		$this->view->disable();
+		parent::initialize();
+	}
+
+	public function indexAction()
+	{
+		if ($this->request->isAjax()) {
+			print_r($this->request->getPost());
+		}
+	}
+}
