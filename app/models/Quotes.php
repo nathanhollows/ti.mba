e81c4c572736c70b7d94cbc7029f0f722ec0b9d3
Wisend <me@nathanhollows.com>
2016-06-10 20:38:29 +1200

    After create and Get actions for model

--- app/models/Quotes.php
+++ app/models/Quotes.php
@@ -53,5 +53,52 @@ class Quotes extends Model
 		$results = parent::count("MONTH(date) = MONTH(NOW()) AND YEAR(date) = YEAR(NOW()) AND sale = 1");
 		return count($results);
 	}
-	
+
+    public function afterCreate()
+    {
+        $auth = new Auth;
+        // Lets make some history
+        $history = new ContactRecord();
+        $history->date = date('Y-m-d');
+        $history->customerCode = $this->customerCode;
+        $history->user = $auth->getId();
+        $history->contactType = 13;
+        $history->contact = $this->id;
+        $history->details = "Quote " . $this->quoteId . " created by " . $auth->getName();
+        $history->save();
+    }
+
+    public function getHot()
+    {
+		$results = parent::find(array(
+			"status = 1",
+		));
+		return $results;
+    }
+
+    public function getWarm()
+    {
+		$results = parent::find(array(
+			"status = 2",
+		));
+		return $results;
+    }
+
+    public function getCold()
+    {
+		$results = parent::find(array(
+			"status = 3",
+		));
+		return $results;
+    }
+
+    public function getDead()
+    {
+		$results = parent::find(array(
+			"status = 4",
+		));
+		return $results;
+
+    }
+
 }
