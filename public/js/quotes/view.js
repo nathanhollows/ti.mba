a60c3df74b27ee98e8db42b5e4b4a7ff058bb2a0
Wisend <me@nathanhollows.com>
2016-06-10 20:42:06 +1200

    No longer needed

--- public/js/quotes/view.js
+++
@@ -1,9 +0,0 @@
-function cloneRow() {
-  var row = document.getElementById("rowToClone"); // find row to copy
-  var table = document.getElementById("items"); // find table to append to
-  var clone = row.cloneNode(true); // copy children too
-  clone.id = "newID"; // change id or other attributes/contents
-  table.appendChild(clone); // add new row to end of table
-  $('.selectpicker').selectpicker('refresh');
-}
-$(document).ready(cloneRow);
