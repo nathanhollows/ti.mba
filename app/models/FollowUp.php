b776cd519f047a4b40a1c32bf2c34f7e4dcd7f24
Wisend <me@nathanhollows.com>
2016-06-10 20:53:21 +1200

    "Get" and "Complete" functionality

--- app/models/FollowUp.php
+++ app/models/FollowUp.php
@@ -78,4 +78,39 @@ class FollowUp extends Model
     	$this->hasOne('followUpUser', 'App\Models\Users', 'id', array('alias' => 'chaseUser'));
     }
 
+    public function getToday($user)
+    {
+        return parent::find(array(
+            "conditions" => "followUpUser = ?1 AND completed is Null AND followUpDate = DATE(NOW())",
+            "bind"  => array(1 => $user)
+        ));
+    }
+
+    public function getOverdue($user)
+    {
+        return parent::find(array(
+            "conditions" => "followUpUser = ?1 AND completed is Null AND followUpDate < DATE(NOW())",
+            "bind"  => array(1 => $user)
+        ));
+    }
+
+    public function getComing($user)
+    {
+        return parent::find(array(
+            "conditions" => "followUpUser = ?1 AND completed is Null AND followUpDate > DATE(NOW())",
+            "bind"  => array(1 => $user)
+        ));
+    }
+
+    public function complete()
+    {
+        $this->completed = date('Y-m-d H:i:s');
+        $success = $this->save();
+        if ($success) {
+            return true;
+        } else {
+            return false;
+        }
+     }
+
 }
