68083ab7c643cb811a19755090d06fd942a7971e
Wisend <me@nathanhollows.com>
2016-07-30 23:12:55 +1200

    Create sales budget model

---
+++ app/models/SalesBudget.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+class SalesBudget extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $year;
+
+    /**
+     *
+     * @var interger
+     */
+    public $month;
+
+    /**
+     *
+     * @var float
+     */
+    public $budget;
+
+    /**
+     *
+     * @var float
+     */
+    public $ytd_budget;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        
+    }
+
+}
