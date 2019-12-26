2b2491afe0975d3d31b6b51a8ab457f292c1faa5
nathanhollows <nathanhollows@gmail.com>
2019-12-27 00:39:50 +1300

    Remove entirely pointless auth check

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -17,9 +17,7 @@ class DashboardController extends ControllerBase
 {
 	public function initialize()
 	{
-        if ($this->session->has('auth-identity')) {
-            $this->view->setTemplateBefore('private');
-        }
+        $this->view->setTemplateBefore('private');
         $this->tag->prependTitle('Dashboard');
         parent::initialize();
 	}
