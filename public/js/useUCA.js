617bfb14da0629501f85b7d477fe4b2f4f42f335
nathanhollows <nathanhollows@gmail.com>
2020-05-08 00:37:44 +1200

    Use UCA (voip client) for making calls on desktop (if enabled)

---
+++ public/js/useUCA.js
@@ -0,0 +1,22 @@
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB10|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+} else {
+    $(function() {
+        $(".tel-link").each(function(){
+            this.href = this.href.replace('tel:', 'DialFromUCA://');
+        });
+    });
+    $('.datatable').on( 'draw.dt', function () {
+        $(function() {
+            $(".tel-link").each(function(){
+                this.href = this.href.replace('tel:', 'DialFromUCA://');
+            });
+        });
+    } );
+    $( document ).ajaxComplete(function() {
+        $(function() {
+            $(".tel-link").each(function(){
+                this.href = this.href.replace('tel:', 'DialFromUCA://');
+            });
+        });
+    });
+}
