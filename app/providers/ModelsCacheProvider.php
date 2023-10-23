45e4e6ad2b415c32a64bf509d97b8aaea5e21891
nathanhollows <me@nathanhollows.com>
2023-10-23 00:47:19 +0000

    Switching from APC to filesystem cache

--- app/providers/ModelsCacheProvider.php
+++ app/providers/ModelsCacheProvider.php
@@ -6,27 +6,28 @@ namespace App\Providers;
 use Phalcon\Di\DiInterface;
 use Phalcon\Di\ServiceProviderInterface;
 use Phalcon\Cache;
-use Phalcon\Cache\AdapterFactory;
 use Phalcon\Di\FactoryDefault;
 use Phalcon\Storage\SerializerFactory;
+use Phalcon\Cache\Adapter\Stream;
 
 class ModelsCacheProvider implements ServiceProviderInterface
 {
     protected $providerName = 'modelsCache';
-
+    
     public function register(DiInterface $di): void
     {
-        $di->set($this->providerName, function () {
+        $di->set($this->providerName, function () use ($di) {
             $serializerFactory = new SerializerFactory();
-            $adapterFactory    = new AdapterFactory($serializerFactory);
-
+            $cacheDir = $di->getShared('config')->path('application.cacheDir') . 'modelsCache/';
+            
             $options = [
                 'defaultSerializer' => 'Php',
-                'lifetime'          => 7200
+                'lifetime'          => 7200,
+                'storageDir'        => $cacheDir,
             ];
-
-            $adapter = $adapterFactory->newInstance('apcu', $options);
-
+            
+            $adapter = new Stream($serializerFactory, $options);
+            
             return new Cache($adapter);
         });
     }
